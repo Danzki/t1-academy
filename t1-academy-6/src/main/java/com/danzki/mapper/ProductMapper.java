efc178ecca01715319ea101650a5d46029d892d3
Dan.Kapustin <dan.kapustin@gmail.com>
2025-07-29 09:10:35 +0300

    Задание 6. Убрал комменты

--- t1-academy-6/src/main/java/com/danzki/mapper/ProductMapper.java
+++ t1-academy-6/src/main/java/com/danzki/mapper/ProductMapper.java
@@ -8,9 +8,7 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
-//    @Mapping(target = "id", ignore = true)
     ProductDto toDto(UserProduct product);
 
-//    @Mapping(target = "id", ignore = true)
     List<ProductDto> toDtos(List<UserProduct> products);
 }

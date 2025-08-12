b386902d47cb79059d446d55f9dcbe326b17cb53
Dan.Kapustin <dan.kapustin@gmail.com>
2025-08-12 19:18:31 +0300

    Задание 8. Фикс настроек для swagger

--- t1-academy-final/src/main/java/com/danzki/LimitApplication.java
+++ t1-academy-final/src/main/java/com/danzki/LimitApplication.java
@@ -3,7 +3,6 @@ package com.danzki;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -18,8 +17,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
                 version = "1.0",
                 description = "API Documentation",
                 contact = @Contact(name = "danzki", email = "danzki@inbox.ru")
-        ),
-        servers = @Server(url = "http://localhost:8083", description = "Local ENV")
+        )
 )
 public class LimitApplication {
     public static void main(String[] args) {

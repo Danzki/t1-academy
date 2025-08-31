f59de21b1bbf108ac9924fd982f1e1cf5ee439c8
Dan.Kapustin <dan.kapustin@gmail.com>
2025-08-31 20:45:17 +0300

    Факультатив (Proto). Реализация сервиса пользователей через Protobuf

---
+++ t1-academy-8/src/main/java/com/danzki/model/User.java
@@ -0,0 +1,34 @@
+package com.danzki.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE,
+            generator="seq_id")
+    @SequenceGenerator(name = "user_seq", sequenceName = "seq_id", allocationSize=1)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
